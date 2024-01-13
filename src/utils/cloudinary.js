5305a7dba4e01af61fa61c7f19cc0b00e79ba285
rizwan <rizwanalamcoc@gmail.com>
2024-01-13 19:13:18 +0530

    added multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from 'cloudinary';
+import { response } from 'express';
+import fs from "fs"
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) =>{
+try {
+    if(!localFilePath)return null
+    cloudinary.uploader.upload(localFilePath, {
+        resource_type: "auto"
+    })
+    console.log("File is uploaded successfully on cloudinary",
+    response.url);
+} catch (error) {
+    fs.unlinkSync(localFilePath)// remove the locally saved File as the upload operation got failed
+    return null;
+}
+}
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
